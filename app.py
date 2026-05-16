be785dfc894bdb0973c2b6801da6c46b4c053807
Souleymane Ongoïba <souleymaneongoiba659@gmail.com>
2026-05-16 00:06:35 +0000

    modification recente

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from functools import wraps
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Secret
+
+
+
+app.secret_key = "iug2026"
+
 # --- AUTHENTICATION ---
 
 def login_required(f):
@@ -504,4 +510,4 @@ def rapports():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- config.py
+++ config.py
@@ -3,4 +3,5 @@ class Config:
     MYSQL_USER = 'bien'
     MYSQL_PASSWORD = 'iug2026'
     MYSQL_DB = 'boutique'
-    SECRET_KEY = 'votre_cle_secrete_ici'
+    SECRET_KEY = 'k8sP2xQ9mL4vT7zA1nR5yW3u' 
+    
